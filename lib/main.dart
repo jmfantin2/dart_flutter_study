bb9f6e19106efe2d4b6d158620c1e2e0e2c0d983
JM Fantin <joao.fantin@acad.pucrs.br>
2019-06-06 12:55:15 -0300

    add save component

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,9 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();   // Add this line.
+  final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
@@ -35,11 +36,16 @@ class RandomWordsState extends State<RandomWords> {
         });
   }
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);  // Add this line.
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(   // Add the lines from here... 
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),                // ... to here.
     );
   }
     
